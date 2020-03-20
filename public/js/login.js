a08e350fe71a6f0fa16643eec2d055dd5676f4d6
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-03-20 18:01:00 +0200

    remove alerts #9

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,7 @@
 const loginButton = document.getElementById('login_btn');
 const email = document.getElementById('email').value;
 const password = document.getElementById('password').value;
+const loginNote = document.getElementById('login-note');
 
 loginButton.addEventListener('click', (e) => {
 	e.preventDefault();
@@ -20,11 +21,20 @@ loginButton.addEventListener('click', (e) => {
 	})
 		.then((response) => response.json())
 		.then((res) => {
-			if (res.error) alert('use valid email or password');
-			else {
-				if (res.message === 'logged in successfully') alert(res.message);
-				else if (res.message === 'your email or password is incorrect')
-					alert(res.message);
+			if (res.error || res.message === 'your email or password is incorrect') {
+				loginNote.innerHTML = 'your email or password is incorrect';
+				if (loginNote.classList.contains('green-note')) {
+					loginNote.classList.remove('green-note');
+				}
+				loginNote.classList.add('red-note');
+			} else {
+				if (res.message === 'logged in successfully') {
+					loginNote.innerHTML = 'logged in successfuly';
+					if (loginNote.classList.contains('red-note')) {
+						loginNote.classList.remove('red-note');
+					}
+					loginNote.classList.add('green-note');
+				}
 			}
 		})
 		.catch((err) => console.log(err));
